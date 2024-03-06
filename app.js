bee201cf9891cc950af74b79bcc5278894498c2a
Dylan Peltier <peltdyla@gmail.com>
2024-03-06 15:00:44 -0500

    Added index.html, added fileserver & changed request responde to point to index.html

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 // create server and write default message
 const server = http.createServer((req, res) => {
-    res.write('Hello Node.js')
-    res.end()
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            res.writeHead(404)
+            res.write('Error 404: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 // listen on designated port
